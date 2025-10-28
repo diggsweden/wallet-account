00d1037f85ded79d6b8e0c992b6b8e515d66285c
Tomas Robertsson <extern.tomas.robertsson@digg.se>
2025-10-28 11:13:03 +0100

    chore: add bare bone spring boot application
    Signed-off-by: Tomas Robertsson <extern.tomas.robertsson@digg.se>

---
+++ src/main/java/se/digg/wallet/account/Application.java
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2025 diggsweden/wallet-account
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+package se.digg.wallet.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
---
+++ src/test/java/se/digg/wallet/account/ApplicationTests.java
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2025 Digg - Agency for Digital Government
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+package se.digg.wallet.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
